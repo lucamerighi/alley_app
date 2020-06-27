323ed98ccdfb3ef3ff8d32058f971bf2d28a3723
Luca Merighi <luca.merighi93@gmail.com>
2020-06-27 16:29:37 +0200

    fix db rewrite

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -3,10 +3,8 @@ import 'package:alley_app/model/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
-  final CollectionReference usersCollection =
-      Firestore.instance.collection('users');
-  final CollectionReference teamsCollection =
-      Firestore.instance.collection('teams');
+  final CollectionReference usersCollection = Firestore.instance.collection('users');
+  final CollectionReference teamsCollection = Firestore.instance.collection('teams');
 
   User _currentUser;
 
@@ -17,27 +15,16 @@ class DatabaseService {
     return _currentUser;
   }
 
-  Future saveUser(String uid, String email, String nome, String cognome,
-      String displayName, Ruolo ruolo) async {
-    return await usersCollection.document(uid).setData({
-      'email': email,
-      'nome': nome,
-      'cognome': cognome,
-      'displayName': displayName,
-      'ruolo': ruolo.toString()
-    });
+  Future saveUser(String uid, String email, String nome, String cognome, String displayName, Ruolo ruolo) async {
+    return await usersCollection.document(uid).setData(
+        {'email': email, 'nome': nome, 'cognome': cognome, 'displayName': displayName, 'ruolo': ruolo.toString()});
   }
 
   Future<String> getIdSquadra(String uid) async {
-    return await usersCollection
-        .document(uid)
-        .get()
-        .then((doc) => doc['idSquadra']);
+    return await usersCollection.document(uid).get().then((doc) => doc['idSquadra']);
   }
 
   Future updateCertificato(DateTime scadenza) async {
-    return await usersCollection
-        .document(currentUser.uid)
-        .setData({'scadenzaCertificato': scadenza});
+    return await usersCollection.document(currentUser.uid).updateData({'scadenzaCertificato': scadenza});
   }
 }
